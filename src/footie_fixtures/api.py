3e73f7a73e7b52374f5937992179557d7b6676a0
Dom Batten <dominic.batten@googlemail.com>
2023-01-05 15:07:01 +0000

    add `add_fixtures` function

---
+++ src/footie_fixtures/api.py
@@ -0,0 +1,51 @@
+"""Public api."""
+
+from typing import List
+from typing import Optional
+from typing import Union
+
+from .calendar_utils import add_event
+from .errors import NoFixturesError
+from .football_api import get_fixtures
+
+
+DEFAULT_LEAGUE_ID = "2"  # Champions League
+
+
+def add_fixtures(
+    league_id: Union[str, int] = DEFAULT_LEAGUE_ID,
+    season: Optional[str] = None,
+    round: Optional[str] = None,
+    invites: Optional[List[str]] = None,
+) -> None:
+    """Add fixtures to the calendar.
+
+    Args:
+        league_id: a league id
+        season: a season, defaults to current year
+        round: an optional round, otherwise the latest round will be used
+        invites: a list of email address to add to the invite list
+
+    Raises:
+        NoFixturesError: when there no fixtures to add
+    """
+    invites = invites or []
+
+    fixtures = get_fixtures(
+        league_id=league_id,
+        season=season,
+        round=round,
+    )
+
+    if not fixtures["fixtures"]:
+        raise NoFixturesError
+
+    league_name = fixtures["league"]["name"]
+
+    for date, teams in fixtures["fixtures"].items():
+        add_event(
+            summary=f"{league_name}: {fixtures['league']['round']}",
+            description="\n".join(teams),
+            start=date,
+            attendees=invites,
+        )
